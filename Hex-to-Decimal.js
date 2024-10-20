e68228fc783f7dca5f4988b4b6be4d1f4f56b238
Kanin Gleb <gleb.kanin@gmail.com>
2024-10-20 17:20:23 +0300

    Complete the function which converts hex number (given as a string) to a decimal number.

---
+++ Hex-to-Decimal.js
@@ -0,0 +1,16 @@
+// ❓DESCRIPTION
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+
+// ⚙️ TESTS
+// assert.strictEqual(hexToDec("1"), 1);
+// assert.strictEqual(hexToDec("a"), 10);
+// assert.strictEqual(hexToDec("10"), 16);
+// assert.strictEqual(hexToDec("FF"), 255);
+// assert.strictEqual(hexToDec("-C"), -12);
+
+// ✅ SOLUTION
+function hexToDec(hexString) {
+    const decimalValue = parseInt(hexString, 16);
+    console.log(decimalValue);
+}
