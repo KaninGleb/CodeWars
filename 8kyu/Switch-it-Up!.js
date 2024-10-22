3a51d26b9d4bcdffe169f222adc47116eaf31fff
Kanin Gleb <gleb.kanin@gmail.com>
2024-10-22 23:30:47 +0300

    Switch it Up! Option 2 added

--- 8kyu/Switch-it-Up!.js
+++ 8kyu/Switch-it-Up!.js
@@ -38,3 +38,9 @@ function switchItUp(number) {
             return 'Nine';
     }
 }
+
+// OPTION 2
+function switchItUp(number) {
+    const numberWords = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+    return numberWords[number];
+}
