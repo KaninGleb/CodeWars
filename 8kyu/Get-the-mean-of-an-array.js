b20022bfb07c475de6d51e426555e7766d8eaef5
Kanin Gleb <gleb.kanin@gmail.com>
2024-10-29 23:37:14 +0300

    Get the mean of an array option 2 added

--- 8kyu/Get-the-mean-of-an-array.js
+++ 8kyu/Get-the-mean-of-an-array.js
@@ -23,6 +23,16 @@ function getAverage(marks) {
 }
 
 
+// OPTION 2
+function getAverage(marks) {
+    let sum = 0;
+    for (let mark of marks) {
+        sum += mark;
+    }
+    return Math.floor(sum / marks.length);
+}
+
+
 console.log(getAverage([2, 2, 2, 2]));
 console.log(getAverage([1, 2, 3, 4, 5]));
 console.log(getAverage([1, 1, 1, 1, 1, 1, 1, 2]));
