a4c0695c7b6c3a5f89f41706b4dad033ab8325d9
Kanin Gleb <gleb.kanin@gmail.com>
2024-10-30 00:10:58 +0300

    Reversed Strings option 1 added

---
+++ 8kyu/Reversed-Strings.js
@@ -0,0 +1,29 @@
+// ❓DESCRIPTION
+// Complete the solution so that it reverses the string passed into it.
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+
+// ⚙️ TESTS
+// assert.strictEqual(solution('world'), 'dlrow');
+// assert.strictEqual(solution('hello'), 'olleh');
+// assert.strictEqual(solution(''), '');
+// assert.strictEqual(solution('h'), 'h');
+
+
+// ✅ SOLUTION
+// OPTION 1
+function solution(str) {
+    let reversed = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
+}
+
+
+
+console.log(solution('world'));
+console.log(solution('hello'));
+console.log(solution(''));
+console.log(solution('h'));
