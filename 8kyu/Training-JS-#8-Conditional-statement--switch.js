d113d23b8ca2ec99ab47b395e92e0b86b47c5c59
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-04 00:33:13 +0300

    Training JS #8: Conditional statement--switch option 2 added

--- 8kyu/Training-JS-#8-Conditional-statement--switch.js
+++ 8kyu/Training-JS-#8-Conditional-statement--switch.js
@@ -21,6 +21,7 @@
 
 
 // ✅ SOLUTION
+// OPTION 1
 function howManydays(month) {
     let days;
     switch (month) {
@@ -41,6 +42,20 @@ function howManydays(month) {
             break;
         default:
             days = 28;
+    }
+    return days;
+}
+
+
+// OPTION 2
+function howManydays(month) {
+    let days;
+    if (month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12) {
+        days = 31;
+    } else if (month === 4 || month === 6 || month === 9 || month === 11) {
+        days = 30;
+    } else if (month === 2) {
+        days = 28;
     }
     return days;
 }
