89b9ce1593abde40f575c2f3cea51e55f398483a
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-05 22:17:37 +0300

    Beginner - Reduce but Grow

---
+++ 8kyu/Beginner-Reduce-but-Grow.js
@@ -0,0 +1,20 @@
+// ❓DESCRIPTION
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+
+// ⚙️ TESTS
+// assert.strictEqual(grow([1, 2, 3]), 6);
+// assert.strictEqual(grow([4, 1, 1, 1, 4]), 16);
+// assert.strictEqual(grow([2, 2, 2, 2, 2, 2]), 64);
+
+
+// ✅ SOLUTION
+// OPTION 1
+function grow(x){
+    let sum = 1;
+    for (let i = 0; i < x.length; i++) {
+        sum *= x[i];
+    }
+    return sum;
+}
