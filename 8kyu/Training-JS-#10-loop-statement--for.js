81528a1999c4193eb01650a5e7a0c1bc0cb28be7
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-06 21:16:13 +0300

    Training JS #10: loop statement --for added

---
+++ 8kyu/Training-JS-#10-loop-statement--for.js
@@ -0,0 +1,37 @@
+// ❓DESCRIPTION
+// The function pickIt accepts 1 parameter, arr, which is an array of numbers. We need to traverse arr by using a for loop. If an element is an odd number, push it to the odd array, and if it's an even number, push it to the evenarray.
+
+// I've defined two arrays odd and even in the function, and also wrote the return statement. Your work is to write a for loop.
+
+
+// ⚙️ TESTS
+// Test.assertSimilar(pickIt([1,2]),[[1],[2]]);
+// Test.assertSimilar(pickIt([1,2,3]),[[1,3],[2]]);
+// Test.assertSimilar(pickIt([3,2,1]),[[3,1],[2]]);
+// Test.assertSimilar(pickIt([10,20,30]),[[],[10,20,30]]);
+// Test.assertSimilar(pickIt([11,21,31]),[[11,21,31],[]]);
+// Test.assertSimilar(pickIt([8,1,5,4,6,1,1]),[[1,5,1,1],[8,4,6]]);
+
+
+// ✅ SOLUTION
+function pickIt(arr) {
+    let odd = [],
+        even = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            even.push(arr[i]);
+        } else {
+            odd.push(arr[i]);
+        }
+    }
+    return [odd, even];
+}
+
+
+// ✅ Checking
+console.log(pickIt([1, 2]));
+console.log(pickIt([1, 2, 3]));
+console.log(pickIt([3, 2, 1]));
+console.log(pickIt([10, 20, 30]));
+console.log(pickIt([11, 21, 31]));
+console.log(pickIt([8, 1, 5, 4, 6, 1, 1]));
