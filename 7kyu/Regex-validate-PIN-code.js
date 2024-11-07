11009c287b5924fa26c3a3ba9ac88d344543abe4
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-07 18:55:21 +0300

    Regex validate PIN code Option 2 added

--- 7kyu/Regex-validate-PIN-code.js
+++ 7kyu/Regex-validate-PIN-code.js
@@ -20,7 +20,8 @@
 
 
 // ✅ SOLUTION
-function validatePIN (pin) {
+// OPTION 1
+function validatePIN(pin) {
     if (pin.length === 4 || pin.length === 6) {
         for (let i = 0; i < pin.length; i++) {
             if (pin[i] < '0' || pin[i] > '9') {
@@ -31,3 +32,22 @@ function validatePIN (pin) {
     }
     return false;
 }
+
+
+// OPTION 2
+function validatePIN(pin) {
+    return /^(\d{4}|\d{6})$/.test(pin)
+}
+
+
+// ✅ Checking
+console.log(validatePIN("1234"));
+console.log(validatePIN("12345"));
+console.log(validatePIN("a234"));
+console.log(validatePIN("0000"));
+console.log(validatePIN("123456"));
+console.log(validatePIN("1234567"));
+console.log(validatePIN("-1234"));
+console.log(validatePIN("1.234"));
+console.log(validatePIN("-1.234"));
+console.log(validatePIN("00000000"));
