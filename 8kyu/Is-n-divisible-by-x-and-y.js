da2b20fb3e67e302d340b09f108e8def9d6b30c9
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-09 22:15:20 +0300

    Is n divisible by x and y? Option 2  added

--- 8kyu/Is-n-divisible-by-x-and-y.js
+++ 8kyu/Is-n-divisible-by-x-and-y.js
@@ -16,12 +16,19 @@
 
 
 // ✅ SOLUTION
+// OPTION 1
 function isDivisible(n, x, y) {
     if (Number.isInteger(n / x) && Number.isInteger(n / y)) {
         return true;
     } else {
         return false;
     }
+}
+
+
+// OPTION 2
+function isDivisible(n, x, y) {
+    return n % x === 0 && n % y === 0;
 }
 
 
