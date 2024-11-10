6641828ff23d3b4021b587175c58d163f1fa338d
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-10 20:42:55 +0300

    Is he gonna survive added

---
+++ 8kyu/Is-he-gonna-survive.js
@@ -0,0 +1,32 @@
+// ❓DESCRIPTION
+// A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+// Return true if yes, false otherwise :)
+
+
+// ⚙️ TESTS
+// assert.strictEqual(hero(10, 5), true);
+// assert.strictEqual(hero(7, 4), false);
+// assert.strictEqual(hero(4, 5), false);
+// assert.strictEqual(hero(100, 40), true);
+// assert.strictEqual(hero(1500, 751), false);
+// assert.strictEqual(hero(0, 1), false);
+
+
+// ✅ SOLUTION
+function hero(bullets, dragons) {
+    if (bullets >= dragons * 2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+// ✅ Checking
+console.log(hero(10, 5));
+console.log(hero(7, 4));
+console.log(hero(4, 5));
+console.log(hero(100, 40));
+console.log(hero(1500, 751));
+console.log(hero(0, 1));
