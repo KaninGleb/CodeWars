dc2f4ed31e7a5eb65ae7698838cd30e83dd4644e
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-10 21:39:38 +0300

    Function 1 - hello world added

---
+++ 8kyu/Function-1-hello-world.js
@@ -0,0 +1,20 @@
+// ❓DESCRIPTION
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+
+// ⚙️ TESTS
+// assert.strictEqual(greet(), "hello world!");
+// assert.strictEqual(typeof greet, "function", "greet should be a function");
+
+
+// ✅ SOLUTION
+function greet() {
+    return 'hello world!';
+}
+
+
+// ✅ Checking
+console.log(greet());
