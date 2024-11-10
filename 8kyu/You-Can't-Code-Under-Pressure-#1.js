33becf1070f60286f910885a940226b45c7357bf
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-10 21:28:07 +0300

    You Can't Code Under Pressure #1 added

---
+++ 8kyu/You-Can't-Code-Under-Pressure-#1.js
@@ -0,0 +1,18 @@
+// ❓DESCRIPTION
+// Code as fast as you can! You need to double the integer and return it.
+
+
+// ⚙️ TESTS
+// Test.assertEquals(doubleInteger(2), 4);
+
+
+// ✅ SOLUTION
+function doubleInteger(i) {
+    i = i * 2;
+    return i;
+}
+
+
+// ✅ Checking
+console.log(doubleInteger(5));
+console.log(doubleInteger(-3));
