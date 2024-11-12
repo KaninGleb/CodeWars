1614cd7644c6060c34646e68afe6dde46e79631b
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-12 21:25:26 +0300

    Odd or Even? added

---
+++ 7kyu/Odd-or-Even.js
@@ -0,0 +1,58 @@
+// ❓DESCRIPTION
+// Task:
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+// Give your answer as a string matching "odd" or "even".
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Examples:
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+
+
+// ⚙️ TESTS
+// assert.strictEqual(oddOrEven([0]), 'even')
+// assert.strictEqual(oddOrEven([1]), 'odd')
+// assert.strictEqual(oddOrEven([]), 'even')
+// assert.strictEqual(oddOrEven([0, 1, 5]), 'even')
+// assert.strictEqual(oddOrEven([0, 1, 3]), 'even')
+// assert.strictEqual(oddOrEven([1023, 1, 2]), 'even')
+// assert.strictEqual(oddOrEven([0, -1, -5]), 'even')
+// assert.strictEqual(oddOrEven([0, -1, -3]), 'even')
+// assert.strictEqual(oddOrEven([-1023, 1, -2]), 'even')
+// assert.strictEqual(oddOrEven([0, 1, 2]), 'odd')
+// assert.strictEqual(oddOrEven([0, 1, 4]), 'odd')
+// assert.strictEqual(oddOrEven([1023, 1, 3]), 'odd')
+// assert.strictEqual(oddOrEven([0, -1, 2]), 'odd')
+// assert.strictEqual(oddOrEven([0, 1, -4]), 'odd')
+// assert.strictEqual(oddOrEven([-1023, -1, 3]), 'odd')
+
+
+// ✅ SOLUTION
+function oddOrEven (array) {
+    const summary = array.reduce((acc, n) => acc + n, 0);
+    return summary % 2 ? 'odd' : 'even';
+}
+
+
+// ✅ Checking
+console.log(oddOrEven([0]));
+console.log(oddOrEven([1]));
+console.log(oddOrEven([]));
+console.log(oddOrEven([0, 1, 5]));
+console.log(oddOrEven([0, 1, 3]));
+console.log(oddOrEven([1023, 1, 2]));
+console.log(oddOrEven([0, -1, -5]));
+console.log(oddOrEven([0, -1, -3]));
+console.log(oddOrEven([-1023, 1, -2]));
+console.log(oddOrEven([0, 1, 2]));
+console.log(oddOrEven([0, 1, 4]));
+console.log(oddOrEven([1023, 1, 3]));
+console.log(oddOrEven([0, -1, 2]));
+console.log(oddOrEven([0, 1, -4]));
+console.log(oddOrEven([-1023, -1, 3]));
