f54f253ce0c332bca157be6049aba1cbf1055855
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-13 23:40:14 +0300

    FIXME: Replace all dots Option 2 added

--- 8kyu/FIXME-Replace-all-dots.js
+++ 8kyu/FIXME-Replace-all-dots.js
@@ -15,10 +15,18 @@
 
 
 // ✅ SOLUTION
+// OPTION 1
 const replaceDots = function (str) {
     return str.replace(/\./g, '-');
 }
 
 
+// OPTION 2
+const replaceDots2 = function (str) {
+    return str.replaceAll(".", "-");
+};
+
+
 // ✅ Checking
 console.log(replaceDots("one.two.three"));
+console.log(replaceDots2("one.two.three"));
