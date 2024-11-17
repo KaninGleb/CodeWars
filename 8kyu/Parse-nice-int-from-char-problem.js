a12663b2fc4a4e30c55c5469f053a6f497b27052
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-17 14:56:45 +0300

    Parse nice int from char problem added

---
+++ 8kyu/Parse-nice-int-from-char-problem.js
@@ -0,0 +1,32 @@
+// ❓DESCRIPTION
+// You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+// Write a program that returns the girl's age (0-9) as an integer.
+
+// Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+
+
+// ⚙️ TESTS
+// assert.strictEqual(getAge("4 years old"), 4);
+// assert.strictEqual(getAge("9 years old"), 9);
+// assert.strictEqual(getAge("1 year old"), 1);  
+
+
+// ✅ SOLUTION
+function getAge(inputString) {
+    const splitString = inputString.split(' ');
+
+    for (const element of splitString) {
+        number = Number(element);
+        
+        if (!isNaN(number)) {
+            return number;
+        }
+    }
+}
+
+
+// ✅ Checking
+console.log(getAge("4 years old"));
+console.log(getAge("9 years old"));
+console.log(getAge("1 year old"));
