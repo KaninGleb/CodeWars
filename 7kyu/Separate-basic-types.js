169718bd509aafe976fc3e85bd3526f0b43d15d3
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-17 15:27:29 +0300

    Separate basic types added

---
+++ 7kyu/Separate-basic-types.js
@@ -0,0 +1,50 @@
+// ❓DESCRIPTION
+// Given: a sequence of different type of values (number, string, boolean). You should return an object with a separate properties for each of types presented in input. Each property should contain an array of corresponding values.
+
+// keep order of values like in input array
+// if type is not presented in input, no corresponding property are expected
+// So, for this input:
+
+// ['a', 1, 2, false, 'b']
+// expected output is:
+
+// {
+//     number: [1, 2],
+//     string: ['a', 'b'],
+//     boolean: [false]
+// }
+
+
+// ⚙️ TESTS
+// assert.deepEqual(separateTypes(['a', 1, 2, false, 'b']), {number: [1, 2],  string: ['a', 'b'], boolean: [false]});
+// assert.deepEqual(separateTypes(['a', 1, 2 ]), {number: [1, 2],  string: ['a']});
+
+
+// ✅ SOLUTION
+function separateTypes(input) {
+    const resultObj = {
+        number: [],
+        string: [],
+        boolean: [],
+        object: []
+    }
+
+    for (let i = 0; i < input.length; i++) {
+        if (typeof input[i] === 'number') {
+            resultObj.number.push(input[i]);
+        } else if (typeof input[i] === 'string') {
+            resultObj.string.push(input[i]);
+        } else if (typeof input[i] === 'boolean') {
+            resultObj.boolean.push(input[i]);
+        } else if (typeof input[i] === 'object') {
+            resultObj.object.push(input[i]);
+        }
+    }
+    return resultObj;
+}
+
+
+// ✅ Checking
+console.log(separateTypes(['a', 1, 2, false, 'b']));
+console.log(separateTypes(['a', 1, 2]));
+console.log(separateTypes([1, 'text', true, {key: 'value'}, 42, 'another text']));
