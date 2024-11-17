b8ae930b263f187c3e331c899e6ad6780b77b3e9
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-17 16:14:22 +0300

    Growth of a Populatio Checking added

--- 7kyu/Growth-of-a-Population.js
+++ 7kyu/Growth-of-a-Population.js
@@ -47,7 +47,7 @@ function nbYear(p0, percent, aug, p) {
 }
 
 
-
+// ✅ Checking
 console.log(nbYear(1500, 5, 100, 5000));
 console.log(nbYear(1500000, 2.5, 10000, 2000000));
 console.log(nbYear(1500000, 0.25, 1000, 2000000));
