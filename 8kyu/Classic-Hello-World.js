19a7c9acbe165a20e71f110e228eef870d10ba4f
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-30 22:39:34 +0300

    Classic Hello World added

--- 8kyu/Classic-Hello-World.js
+++ 8kyu/Classic-Hello-World.js
@@ -4,8 +4,8 @@
 // Note that for some languages, the function main is the entry point of the program.
 
 // Here's how it will be tested:
+// Solution.main("parameter1","parameter2");
 
-//     Solution.main("parameter1","parameter2");
 // Hints:
 // 1. Check your references
 // 2. Think about the scope of your method
