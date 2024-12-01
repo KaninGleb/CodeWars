84b743414f8ced3960f6d7965b57f8de7e5b27fc
Kanin Gleb <gleb.kanin@gmail.com>
2024-12-01 03:53:38 +0300

    Ones and Zeros Option 2 added

--- 7kyu/Ones-and-Zeros.js
+++ 7kyu/Ones-and-Zeros.js
@@ -23,12 +23,25 @@
 
 
 // ✅ SOLUTION
+// OPTION 1
 const binaryArrayToNumber = arr => {
     let result = 0;
 
     for (let i = 0; i < arr.length; i++) {
         result = result * 2 + arr[i];
     }
+    return result;
+};
+
+
+// OPTION 2
+const binaryArrayToNumber = arr => {
+    let result = 0;
+
+    arr.forEach(arr => {
+        result = result * 2 + arr;
+    })
+
     return result;
 };
 
