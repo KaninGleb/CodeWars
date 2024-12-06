9959919e3d6e8073b6c911461607495fdcd7ae1d
Kanin Gleb <gleb.kanin@gmail.com>
2024-12-06 13:47:18 +0300

    Find the odd int Option 2 added

--- 6kyu/Find-the-odd-int.js
+++ 6kyu/Find-the-odd-int.js
@@ -26,6 +26,7 @@
 
 
 // ✅ SOLUTION
+// OPTION 1
 const findOdd = arr => {
     const oddObj = {};
 
@@ -41,6 +42,10 @@ const findOdd = arr => {
 }
 
 
+// OPTION 2
+const findOdd2 = arr => arr.reduce((acc, num) => acc ^ num, 0);
+
+
 // ✅ Checking
 console.log(findOdd([7]));
 console.log(findOdd([0]));
@@ -53,3 +58,15 @@ console.log(findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]));
 console.log(findOdd([10]));
 console.log(findOdd([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1]));
 console.log(findOdd([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10]));
+
+console.log(findOdd2([7]));
+console.log(findOdd2([0]));
+console.log(findOdd2([1, 1, 2]));
+console.log(findOdd2([0, 1, 0, 1, 0]));
+console.log(findOdd2([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
+console.log(findOdd2([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]));
+console.log(findOdd2([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5]));
+console.log(findOdd2([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5]));
+console.log(findOdd2([10]));
+console.log(findOdd2([1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1]));
+console.log(findOdd2([5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10]));
