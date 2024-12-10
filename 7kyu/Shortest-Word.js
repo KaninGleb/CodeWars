c956bcbefa0cc8d08367b07ba98ced24cff96c0b
Kanin Gleb <gleb.kanin@gmail.com>
2024-12-10 14:50:25 +0300

    Shortest Word Option 2 added

--- 7kyu/Shortest-Word.js
+++ 7kyu/Shortest-Word.js
@@ -10,6 +10,7 @@
 
 
 // ✅ SOLUTION
+// OPTION 1
 const findShort = s => {
     const splitS = s.split(' ');
     let minLength = splitS[0].length;
@@ -24,7 +25,15 @@ const findShort = s => {
 }
 
 
+// OPTION 2
+const findShort2 = s => Math.min(...s.split(' ').map(s => s.length));
+
+
 // ✅ Checking
 console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
 console.log(findShort("turns out random test cases are easier than writing out basic ones"));
 console.log(findShort("Let's travel abroad shall we"));
+
+console.log(findShort2("bitcoin take over the world maybe who knows perhaps"));
+console.log(findShort2("turns out random test cases are easier than writing out basic ones"));
+console.log(findShort2("Let's travel abroad shall we"));
