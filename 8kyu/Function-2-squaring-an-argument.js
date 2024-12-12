872a5c3a91d6993ed358904195a898a7a8bd90ee
Kanin Gleb <gleb.kanin@gmail.com>
2024-12-12 22:02:32 +0300

    Function 2 - squaring an argument added

---
+++ 8kyu/Function-2-squaring-an-argument.js
@@ -0,0 +1,14 @@
+// ❓DESCRIPTION
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+// ⚙️ TESTS
+// assert.strictEqual(square(3), 9);
+
+
+// ✅ SOLUTION
+const square = n => n * n;
+
+
+// ✅ Checking
+console.log((square(3)));
