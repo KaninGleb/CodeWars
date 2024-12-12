086533cd9a9e555d41de43d30e587c787512a766
Kanin Gleb <gleb.kanin@gmail.com>
2024-12-12 22:15:15 +0300

    Testing 1-2-3 added

---
+++ 7kyu/Testing-1-2-3.js
@@ -0,0 +1,31 @@
+// ❓DESCRIPTION
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+//
+// Examples: (Input --> Output)
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+
+// ⚙️ TESTS
+// assert.deepEqual(number([]), [], 'Empty array should return empty array');
+// assert.deepEqual(number(["a", "b", "c"]), ["1: a", "2: b", "3: c"], 'Return the correct line numbers');
+
+
+// ✅ SOLUTION
+// OPTION 1
+const number = array => {
+    const result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        result.push(`${i + 1}: ${array[i]}`);
+    }
+    return result;
+}
+
+
+
+// ✅ Checking
+console.log(number([]));
+console.log(number(["a", "b", "c"]));
