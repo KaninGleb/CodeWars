8fcaea64cb12cef2bade7d322ed0feac07e0b0ee
Kanin Gleb <gleb.kanin@gmail.com>
2024-12-29 15:14:15 +0300

    5 without numbers !! added

---
+++ 8kyu/5-without-numbers.js
@@ -0,0 +1,16 @@
+// ❓DESCRIPTION
+// Write a function that always returns 5
+//
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+
+// ⚙️ TESTS
+// assert.strictEqual(unusualFive(), 5);
+
+
+// ✅ SOLUTION
+const unusualFive = () => 'array'.length;
+
+
+// ✅ Checking
+console.log(unusualFive(), '=== 5');
