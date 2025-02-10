72cb3536afb04727ad172d489defd4d62fb71ae3
Kanin Gleb <gleb.kanin@gmail.com>
2025-02-10 15:06:04 +0300

    Strong Number (Special Numbers Series #2) added

---
+++ 7kyu/Strong-Number-(Special-Numbers-Series-2).ts
@@ -0,0 +1,44 @@
+// ❓DESCRIPTION
+// Strong number is a number with the sum of the factorial of its digits is equal to the number itself.
+//
+// For example, 145 is strong, because 1! + 4! + 5! = 1 + 24 + 120 = 145.
+//
+// Task:
+// Given a positive number, find if it is strong or not, and return either "STRONG!!!!" or "Not Strong !!".
+//
+// Examples:
+// 1 is a strong number, because 1! = 1, so return "STRONG!!!!".
+// 123 is not a strong number, because 1! + 2! + 3! = 9 is not equal to 123, so return "Not Strong !!".
+// 145 is a strong number, because 1! + 4! + 5! = 1 + 24 + 120 = 145, so return "STRONG!!!!".
+// 150 is not a strong number, because 1! + 5! + 0! = 122 is not equal to 150, so return "Not Strong !!".
+
+
+// ⚙️ TESTS
+// assert.strictEqual(strong(1), 'STRONG!!!!');
+// assert.strictEqual(strong(2), 'STRONG!!!!');
+// assert.strictEqual(strong(145), 'STRONG!!!!');
+// assert.strictEqual(strong(7), 'Not Strong !!');
+// assert.strictEqual(strong(93), 'Not Strong !!');
+// assert.strictEqual(strong(185), 'Not Strong !!');
+
+
+// ✅ SOLUTION
+function factorial (n: number): number {
+    return n ? n * factorial(n - 1) : 1;
+}
+
+export const strongNumber = (num: number): string => {
+    const numStr = (num + "").split('').map(Number);
+    const strongCheck = num === numStr.reduce((a, b) => a + factorial(b), 0)
+
+    return strongCheck ? 'STRONG!!!!' : 'Not Strong !!';
+}
+
+
+// ✅ Checking
+console.log(strongNumber(1) === 'STRONG!!!!');
+console.log(strongNumber(2) === 'STRONG!!!!');
+console.log(strongNumber(145) === 'STRONG!!!!');
+console.log(strongNumber(7) === 'Not Strong !!');
+console.log(strongNumber(93) === 'Not Strong !!');
+console.log(strongNumber(185) === 'Not Strong !!');
