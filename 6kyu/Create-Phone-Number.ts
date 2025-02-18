09a134bcde6025986c090c54eafccd377af94690
Kanin Gleb <gleb.kanin@gmail.com>
2025-02-18 19:47:31 +0300

    Create Phone Number Option 2 added

--- 6kyu/Create-Phone-Number.ts
+++ 6kyu/Create-Phone-Number.ts
@@ -15,8 +15,20 @@
 
 
 // ✅ SOLUTION
+// OPTION 1
 export function createPhoneNumber(nums: number[]): string {
     return `(${nums[0]}${nums[1]}${nums[2]}) ${nums[3]}${nums[4]}${nums[5]}-${nums[6]}${nums[7]}${nums[8]}${nums[9]}`;
+}
+
+
+// OPTION 2
+export function createPhoneNumber2 (nums: number[]): string {
+    let phoneNumber = '(xxx) xxx-xxxx';
+
+    for (let i = 0; i < nums.length; i++) {
+        phoneNumber = phoneNumber.replace('x', nums[i].toString());
+    }
+    return phoneNumber;
 }
 
 
@@ -24,3 +36,7 @@ export function createPhoneNumber(nums: number[]): string {
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "--- (123) 456-7890");
 console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "--- (111) 111-1111");
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "--- (123) 456-7890");
+
+console.log(createPhoneNumber2([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "--- (123) 456-7890");
+console.log(createPhoneNumber2([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "--- (111) 111-1111");
+console.log(createPhoneNumber2([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "--- (123) 456-7890");
