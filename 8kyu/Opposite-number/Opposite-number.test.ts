7d7dbce4a11611dc1a16b75f0ee16b0eef0abf35
Kanin Gleb <gleb.kanin@gmail.com>
2025-02-20 17:29:33 +0300

    Opposite numbers tests supplemented

--- 8kyu/Opposite-number/Opposite-number.test.ts
+++ 8kyu/Opposite-number/Opposite-number.test.ts
@@ -2,8 +2,12 @@ import {expect, test} from 'vitest';
 import {Kata} from './Opposite-number';
 
 test('Opposite numbers', () => {
+    expect(Kata.opposite(0)).toBe(-0);
     expect(Kata.opposite(1)).toBe(-1);
+    expect(Kata.opposite(-5)).toBe(5);
     expect(Kata.opposite(14)).toBe(-14);
     expect(Kata.opposite(-34)).toBe(34);
-    expect(Kata.opposite(0)).toBe(-0);
+    expect(Kata.opposite(4.25)).toBe(-4.25);
+    expect(Kata.opposite(3.3333333)).toBe(-3.3333333);
+    expect(Kata.opposite(-12525220.3325)).toBe(12525220.3325);
 })
