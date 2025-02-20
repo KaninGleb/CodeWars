38f79ed6fcae6bedfb6c10bb20333757e831a418
Kanin Gleb <gleb.kanin@gmail.com>
2025-02-20 17:05:08 +0300

    Opposite number added

---
+++ 8kyu/Opposite-number.ts
@@ -0,0 +1,23 @@
+// ❓DESCRIPTION
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+//
+// Examples:
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+// ⚙️ TESTS
+// assert.strictEqual(solution.Kata.opposite(1), -1)
+
+
+// ✅ SOLUTION
+export class Kata {
+    static opposite(n: number) {
+        return -n;
+    }
+}
+
+
+// ✅ Checking
+console.log(Kata.opposite(1), -1)
