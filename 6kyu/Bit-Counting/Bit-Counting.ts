ec650ae1a4a6041d4175adc522b513104cb091c7
Kanin Gleb <gleb.kanin@gmail.com>
2025-02-24 22:07:44 +0300

    Bit Counting added

---
+++ 6kyu/Bit-Counting/Bit-Counting.ts
@@ -0,0 +1,22 @@
+// ❓DESCRIPTION
+// Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+//
+// Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+
+// ⚙️ TESTS
+// assert.equal(countBits(0), 0);
+// assert.equal(countBits(4), 1);
+// assert.equal(countBits(7), 3);
+// assert.equal(countBits(9), 2);
+// assert.equal(countBits(10), 2);
+
+
+// ✅ SOLUTION
+export function countBits(n: number): number {
+    return n
+        .toString(2)
+        .split('')
+        .map(Number)
+        .reduce((a, b) => a + b);
+}
