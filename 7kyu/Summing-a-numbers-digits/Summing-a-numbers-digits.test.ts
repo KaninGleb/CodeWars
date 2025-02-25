97af2ef2b7aa3be35e96dcf9e550e11049cd1f0e
Kanin Gleb <gleb.kanin@gmail.com>
2025-02-26 00:04:31 +0300

    Tests for Summing a number's digits added

--- 7kyu/Summing-a-numbers-digits/Summing-a-numbers-digits.test.ts
+++ 7kyu/Summing-a-numbers-digits/Summing-a-numbers-digits.test.ts
@@ -1,7 +1,7 @@
 import {assert, test} from 'vitest';
 import {sumDigits} from './Summing-a-numbers-digits';
 
-test('', () => {
+test('Summing a number\'s digits', () => {
     assert.equal(sumDigits(10), 1);
     assert.equal(sumDigits(99), 18);
     assert.equal(sumDigits(-32), 5);
