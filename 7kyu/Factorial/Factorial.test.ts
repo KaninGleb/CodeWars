91c9e449f143e48917d2330bb0b842a57c0591b5
Kanin Gleb <gleb.kanin@gmail.com>
2025-03-10 23:56:26 +0300

    Tests for Factorial added

---
+++ 7kyu/Factorial/Factorial.test.ts
@@ -0,0 +1,19 @@
+import {expect, test} from 'vitest';
+import {factorial} from './Factorial';
+
+
+test('Factorial', () => {
+    expect(factorial(0)).toBe(1);
+    expect(factorial(1)).toBe(1);
+    expect(factorial(2)).toBe(2);
+    expect(factorial(3)).toBe(6);
+    expect(factorial(4)).toBe(24);
+    expect(factorial(5)).toBe(120);
+    expect(factorial(6)).toBe(720);
+    expect(factorial(7)).toBe(5040);
+    expect(factorial(8)).toBe(40320);
+    expect(factorial(9)).toBe(362880);
+    expect(factorial(10)).toBe(3628800);
+    expect(factorial(11)).toBe(39916800);
+    expect(factorial(12)).toBe(479001600);
+})
