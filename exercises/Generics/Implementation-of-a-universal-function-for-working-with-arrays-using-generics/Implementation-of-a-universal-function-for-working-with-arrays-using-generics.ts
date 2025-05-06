408e11c737fc1a3a7c933a2f913926a797545cfc
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-06 22:43:42 +0300

    Exercises - Implementation of a universal function for working with arrays using generics added

---
+++ exercises/Generics/Implementation-of-a-universal-function-for-working-with-arrays-using-generics/Implementation-of-a-universal-function-for-working-with-arrays-using-generics.ts
@@ -0,0 +1,26 @@
+// ❓DESCRIPTION
+// Необходимо создать дженериковую функцию, принимающую массив любого типа и значение того же типа.
+// Функция должна вернуть массив без изменений, если значение уже есть в массиве,
+// а если нет - новый массив с добавленным значением.
+//
+// Требования:
+// функция должна быть дженериковой и работать с массивами любого типа;
+// для проверки наличия элемента в массиве используйте метод includes;
+// типы массива и элемента должны быть связаны через дженерики;
+// функция должна быть чистой (не должна изменять оригинальный массив):
+
+
+// ⚙️ TESTS
+// // Строки
+// const stringArray = ["apple", "banana", "cherry"]
+// console.log(updateArray(stringArray, "banana")) // ['apple', 'banana', 'cherry']
+// console.log(updateArray(stringArray, "strawberry")) // ['apple', 'banana', 'cherry', 'strawberry']
+//
+// // Числа
+// const numberArray = [1, 2, 3]
+// console.log(updateArray(numberArray, 2)) // [1, 2, 3]
+// console.log(updateArray(numberArray, 4)) // [1, 2, 3, 4]
+
+
+// ✅ SOLUTION
+export const updateArray = <T>(arr: T[], value: T): T[] => arr.includes(value) ? arr : [...arr, value]
